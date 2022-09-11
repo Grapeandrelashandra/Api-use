8906ebe1ce6ac788709728b2bc35cddc1105cd89
Grapeandrelashandra <91314971+Grapeandrelashandra@users.noreply.github.com>
2022-09-11 17:50:46 +0200

    php

---
+++ Current/assets/php/SessionStart.php
@@ -0,0 +1,18 @@
+<?php
+// Start the session
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// Set session variables
+$_SESSION["UserID"] = "green";
+$_SESSION["UserType"] = "cat";
+
+
+?>
+
+</body>
+</html>
