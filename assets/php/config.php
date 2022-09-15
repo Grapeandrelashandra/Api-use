c7b8f15742fa9a517e207d06f2588c0d7815200f
Thabang <thabangmariks@gmail.com>
2022-09-15 13:28:57 +0200

    cleardb config

--- assets/php/config.php
+++ assets/php/config.php
@@ -1,6 +1,6 @@
 <?php
-    define("SERVERNAME", "127.0.0.1");
-    define("USERNAME", "root");
-    define("PASSWORD",  "");
-    define("DATABASE", "epi_use");
+    define("SERVERNAME", "eu-cdbr-west-03.cleardb.net");
+    define("USERNAME", "bf8e658003a85a");
+    define("PASSWORD",  "ad9a40a9");
+    define("DATABASE", "heroku_8c8c7baf0e9095a");
 ?>
